7727433950cdadaff8d7e5e0aa70b1d702963ea9
Jeremy Berman <jerber@sas.upenn.edu>
2023-10-26 12:16:21 -0400

    better exports

--- fastgql/__init__.py
+++ fastgql/__init__.py
@@ -1,5 +1,13 @@
+from .gql_models import GQL, GQLInput, GQLError, GQLConfigDict
 from .schema_builder import SchemaBuilder
 
 build_router = SchemaBuilder.build_router
 
-__all__ = ["SchemaBuilder", "build_router"]
+__all__ = [
+    "SchemaBuilder",
+    "build_router",
+    "GQL",
+    "GQLInput",
+    "GQLError",
+    "GQLConfigDict",
+]
