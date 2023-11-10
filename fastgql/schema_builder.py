d56958cbf091c375524d897c5b7f3ddbc37a7144
Jeremy Berman <jerber@sas.upenn.edu>
2023-11-10 16:28:01 -0500

    adding introspection case for postman

--- fastgql/schema_builder.py
+++ fastgql/schema_builder.py
@@ -556,7 +556,10 @@ class SchemaBuilder:
                     status_code=status.HTTP_400_BAD_REQUEST,
                 )
 
-            if request_data.operation_name == "IntrospectionQuery":
+            if (
+                request_data.operation_name == "IntrospectionQuery"
+                or "IntrospectionQuery" in request_data.query
+            ):
                 res = await graphql.graphql(
                     schema=self.schema,
                     source=request_data.query,
